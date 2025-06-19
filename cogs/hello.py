8a65e484ab9abc45dcd9008b5f096475d531ce8b
gr3i <girlbeastm@gmail.com>
2025-06-19 23:08:55 +0200

    Some changes

--- cogs/hello.py
+++ cogs/hello.py
@@ -5,9 +5,9 @@ class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="Zprava", description="Nic ti neřeknu")
+    @app_commands.command(name="hello", description="Řeknu ti čau!")
     async def hello(self, interaction: Interaction):
-            await interaction.response.send_message("Nebo možná.. jo", ephemeral=False)
+        await interaction.response.send_message("Čau!", ephemeral=False)
 
 async def setup(bot):
     await bot.add_cog(Hello(bot))
