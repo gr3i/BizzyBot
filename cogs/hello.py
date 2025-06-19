959e295af5b7bb5a1e1e514d2b3e4df3554b6a72
gr3i <girlbeastm@gmail.com>
2025-06-19 19:57:22 +0200

    try to edit and upload

--- cogs/hello.py
+++ cogs/hello.py
@@ -5,9 +5,9 @@ class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="hello", description="Řeknu ti ahoj!")
+    @app_commands.command(name="hello", description="Řeknu ti něco!")
     async def hello(self, interaction: Interaction):
-        await interaction.response.send_message("Ahoj!", ephemeral=False)
+        await interaction.response.send_message("Toaster je god", ephemeral=False)
 
 async def setup(bot):
     await bot.add_cog(Hello(bot))
