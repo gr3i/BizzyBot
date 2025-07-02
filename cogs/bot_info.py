699bd264973f4ee66f7a4b3415c720cbd5c26e38
gr3i <girlbeastm@gmail.com>
2025-07-02 19:04:37 +0200

    nove BotInfo

--- cogs/bot_info.py
+++ cogs/bot_info.py
@@ -5,15 +5,10 @@ class BotInfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Slash příkaz
     @app_commands.command(name="botinfo", description="Odkaz na repozitář bota.")
-    async def bot_info(self, interaction: Interaction):
+    async def botinfo(self, interaction: Interaction):
         await interaction.response.send_message("https://github.com/gr3i/BizzyBot", ephemeral=False)
 
-    # Automatická registrace slash příkazu po načtení cogu
-    async def cog_load(self):
-        self.bot.tree.add_command(self.bot_info)
-
-# Setup funkce pro dynamické načítání cogu
 async def setup(bot):
     await bot.add_cog(BotInfo(bot))
+
