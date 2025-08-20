e4ef13fd206ba0de34b6c21a917256b1fe51fc46
gr3i <girlbeastm@gmail.com>
2025-08-20 21:19:56 +0200

    changes

--- db/models.py
+++ db/models.py
@@ -1,9 +1,9 @@
 # db/models.py
+from datetime import datetime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean, DateTime, func
 
 class Base(DeclarativeBase):
-    """Zaklad pro vsechny ORM modely."""
     pass
 
 class Verification(Base):
@@ -14,6 +14,7 @@ class Verification(Base):
     mail: Mapped[str] = mapped_column(String, nullable=False)
     verification_code: Mapped[str] = mapped_column(String, nullable=False)
     verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    # timestamp necham vytvorit databazi (CURRENT_TIMESTAMP)
-    created_at: Mapped["datetime"] = mapped_column(DateTime, server_default=func.current_timestamp())
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.current_timestamp(), nullable=False
+    )
 
