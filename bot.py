69aec865ea2368eca47fd3273890a850ec3595a8
gr3i <girlbeastm@gmail.com>
2026-01-29 21:55:12 +0100

    changes

--- bot.py
+++ bot.py
@@ -339,10 +339,10 @@ async def setup_hook():
     for ext in [
         "cogs.hello",
         "cogs.botInfo",
-        #"cogs.verify",
+        "cogs.verify",
         "cogs.role",
         "cogs.reviews",
-        "utils.vyber_oboru",
+        #"utils.vyber_oboru",
         "utils.nastav_prava",
         "cogs.welcome_todo",
         "cogs.pozvanka",
