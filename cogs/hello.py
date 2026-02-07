b7f9209d016bfa0b76a67ac1ac9d41a45e283ce3
Blixyk <girlbeastm@gmail.com>
2026-02-07 17:08:05 +0100

    changes

--- cogs/hello.py
+++ cogs/hello.py
@@ -7,7 +7,7 @@ class Hello(commands.Cog):
 
     @app_commands.command(name="hello", description="Řeknu ti Ahoj")
     async def hello(self, interaction: Interaction):
-        await interaction.response.send_message("Ahoj <:koteseni~1:1469718679309779054>", ephemeral=False)
+        await interaction.response.send_message("Ahoj <:koteseni:1361038813719302175>", ephemeral=False)
 
 async def setup(bot):
     await bot.add_cog(Hello(bot))
